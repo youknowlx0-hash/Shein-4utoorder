6c4098902346674e28e5bf0505a18df298da8a51
youknowlx0-hash <youknowlx0@gmail.com>
2026-02-13 22:13:08 +0530

    Update and rename channels.txt to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# ===== Bot Settings =====
+BOT_TOKEN = "7727357230:AAGl2xQKcX_Y91_NzshJRSU4eTAYfZZVfO8"   # Apna Telegram Bot Token
+ADMIN_ID = 7702942505                # Apna Telegram ID
+
+# ===== Channels List =====
+CHANNELS = [
+    "@Shein_Reward",
+    "@SheinStockss",
+    "@SheinRewardsGc"
+    "@sheinlinks202",
+    "@sheinverse22",
+    "@sheinverse052"
+]
